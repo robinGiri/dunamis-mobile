59d2efa513938a2310ac11b89fe82ea607404299
robinGiri <girirobin2018@gmail.com>
2024-12-10 18:20:15 +0545

    dashboard added

---
+++ lib/view/dashboard_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "Welcome to the Dashboard!",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
