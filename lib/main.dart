8ac4229ec55736a08d2ddea92779b873de0fbfc0
robinGiri <girirobin2018@gmail.com>
2024-12-12 19:08:05 +0545

    feat: OnboardingPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
+import 'package:dunamis/view/OnboardingPage.dart';
 import 'package:flutter/material.dart';
-import 'view/login_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Dunamis Learning App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginPage(),
+      home: OnboardingPage(),
     );
   }
 }
